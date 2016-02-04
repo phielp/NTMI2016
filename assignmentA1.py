657571c781ba96b19d3b21a1d5fa400638064ab3
Kajzzer <k.d.meijer17@gmail.com>
2016-02-04 17:48:45 +0100

    Update assignmentA1.py
    The program also prints the sum of the frequencies of the sequences.

--- assignmentA1.py
+++ assignmentA1.py
@@ -10,22 +10,25 @@ def split_into_array(string):
 
 def counter_list(list):
 	model = Counter() #makes decitonaty 0, if not exists
+	sumOfFreq = 0
 	if args.n == 1:
 		for i in list:
-			model[i] += 1		
+			model[i] += 1
+			sumOfFreq += 1		
 	elif args.n == 2:
 		for i in range(0,len(list) -1):
 			string = list[i] + " " + list[i+1]
 			model[string] += 1
+			sumOfFreq += 1
 	elif args.n == 3:
 		for i in range(0,len(list) - 2):
 			string = list[i] + " " + list[i+1] + " " + list[i+2]
 			model[string] += 1
+			sumOfFreq += 1
 	else:
 		print("to much ngrams")
+	print("Sum of frequencies of sequences: " + str(sumOfFreq))
 	return model
-
-
 
 if __name__ == '__main__':
 
@@ -45,3 +48,4 @@ if __name__ == '__main__':
 	model = counter_list(list)
 
 	print(Counter(model).most_common(args.m))
+	
