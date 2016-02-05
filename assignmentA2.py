91906ab6405ddfe748b0fce12c38ede8a5409b38
legiadan <d.dijkzeul4@gmail.com>
2016-02-05 11:39:36 +0100

    test

--- assignmentA2.py
+++ assignmentA2.py
@@ -4,6 +4,10 @@ from collections import Counter
 import re
 import argparse
 
+def split_into_array(string):
+	words = re.split("\W+ ", string) #splits  words
+	return words
+
 
 if __name__ == '__main__':
 
@@ -13,8 +17,8 @@ if __name__ == '__main__':
 	args = parser.parse_args()
 
 	myfile = open(args.file, 'r') 
-	text = myfile.read().replace('\n\n', '</S> \n <S>')
+	text = myfile.read().replace('\n\n', '</S>  <S>')
 
-	text.write()
+	array = split_into_array(text)	
 
-	print(text)
+	print(array[1:100])
