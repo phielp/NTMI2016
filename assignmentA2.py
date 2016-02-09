833f7398173f516c9ab9d6c2baa4fb0e3a8cbed2
legiadan <d.dijkzeul4@gmail.com>
2016-02-09 17:05:04 +0100

    update 1&2&3

--- assignmentA2.py
+++ assignmentA2.py
@@ -17,10 +17,29 @@ def counter_list(unigram, n):
 	model = Counter(zip(*[unigram[i:] for i in range(n)]))
 	return model
 
+def get_prop_array(prop_file,n):
+	prop_file = open(prop_file, 'r') 
+	
+	prop_read = prop_file.read().replace('\n', ' </s>'*(n-1) + '><'+ '<s> '*(n-1))
+		
+	prop_array = split_into_array(prop_read) 
+
+	if prop_array[1] == '</s>': 
+		del prop_array[0:(n)]
+	if prop_array[len(prop_array)-2] == '<s>':
+		print("test")
+		del prop_array[(len(prop_array)-n)+1:len(prop_array)]
+		
+	prop_array = [prop_array[i:n+i] for i in range(len(prop_array)-(n))]
+
+	return prop_array
+
+
 def calc_prop(prop_array, model_n, model_n_min_one, n):
 	
 
-	prop_dict = {}
+	prop_value = 1
+	prop_value_array = []
 
 	for i in prop_array:
 		prop_n = model_n[tuple(i)]
@@ -29,15 +48,20 @@ def calc_prop(prop_array, model_n, model_n_min_one, n):
 			prop_w = prop_n/prop_n_min_one 
 		else:
 			prop_w = 0
+		if i[0:2] == ["</s>","<s>"]:
+			
+			prop_value_array.append(prop_value)
+			prop_value = 1
+			prop_w = 1
+		prop_value *= prop_w
 
-		prop_dict[tuple(i)] = prop_w
-
-	return prop_dict
+	prop_value_array.append(prop_value)	
+	return prop_value_array
 
 
 if __name__ == '__main__':
 
-	parser = argparse.ArgumentParser(add_help=False)
+	parser = argparse.ArgumentParser(add_help = False)
 
 	parser.add_argument("-corpus",  dest = 'corpus')
 	parser.add_argument("-n", type = int)
@@ -46,28 +70,29 @@ if __name__ == '__main__':
 	parser.add_argument("-scored-permutations", dest = 'perm_file')
 	args = parser.parse_args()
 
-
 	corpus = open(args.corpus, 'r') 
 	
 	corpus_read = corpus.read().replace('\n\n', ' </s>'*(args.n-1) + '><'+ '<s> '*(args.n-1))
 
-	prop_file = open(args.prop_file, 'r') 
-	
-	prop_read = prop_file.read().replace('\n', ' </s>'*(args.n-1) + '><'+ '<s> '*(args.n-1))
-	
 	corpus_array = split_into_array(corpus_read)
 
 	model_n = counter_list(corpus_array,args.n)
 
 	model_n_min_one = counter_list(corpus_array,(args.n-1))
 
-	prop_array = split_into_array(prop_read) 
-
-	del prop_array[0:(args.n)]
-	del prop_array[(len(prop_array)-args.n):len(prop_array)]
-	# TODO: Make list in list split on <s>
+	if args.prop_file:
+		prop_array = get_prop_array(args.prop_file, args.n)
+		print(calc_prop(prop_array[args.n-1:args.n], model_n, model_n_min_one, args.n))
+	elif args.seq_file:
+		prop_array = get_prop_array(args.seq_file, args.n)
+		print(calc_prop(prop_array, model_n, model_n_min_one, args.n))
+	elif args.perm_file:
+		print("niks")
+	else:
+		print(Counter(model_n).most_common(10)) 
+		print(Counter(model_n_min_one).most_common(10)) 
 	
-	prop_array = [prop_array[i:args.n+i] for i in range(len(prop_array)-(args.n))]
+
 
 	# print(prop_array)
 
@@ -88,10 +113,6 @@ if __name__ == '__main__':
 	perm_sen = [start*(args.n-1) + perms[i] + end*(args.n-1) for i in range(len(perms))]
 
 	print(perm_sen)
-	print(len(per_sen))
+	print(len(perm_sen))
 
-	# # print(Counter(model_n).most_common(10)) 
-	#For part 1
-
-	# print(Counter(model_n_min_one).most_common(10)) 
-	#For part 1
+	
