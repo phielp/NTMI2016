2226d76ded98d2aaa016eb5eaaedb23d086a989a
Kaj Meijer <k.d.meijer17@gmail.com>
2016-02-09 15:21:04 +0100

    Beginning of assignmentA2 part 4 (for real)

--- assignmentA2.py
+++ assignmentA2.py
@@ -34,6 +34,7 @@ def calc_prop(prop_array, model_n, model_n_min_one, n):
 
 	return prop_dict
 
+
 if __name__ == '__main__':
 
 	parser = argparse.ArgumentParser(add_help=False)
@@ -45,13 +46,14 @@ if __name__ == '__main__':
 
 	corpus = open(args.corpus, 'r') 
 	# corpus_read = corpus.read().replace('\n', ' ') For part 2
-	corpus_read = corpus.read().replace('\n\n', ' </s>><<s> ')
+	corpus_read = corpus.read().replace('\n\n', ' </s>'*(args.n-1) + '><'+ '<s> '*(args.n-1))
 
 
 	prop_file = open(args.prop_file, 'r') 
+	
 	# prop_read = prop_file.read().replace('\n', ' ') For part 2
-	prop_read = prop_file.read().replace('\n', ' </s>><<s> ')
-
+	prop_read = prop_file.read().replace('\n', ' </s>'*(args.n-1) + '><'+ '<s> '*(args.n-1))
+	
 	corpus_array = split_into_array(corpus_read)
 
 	model_n = counter_list(corpus_array,args.n)
@@ -60,28 +62,34 @@ if __name__ == '__main__':
 
 	prop_array = split_into_array(prop_read) 
 
+	del prop_array[0:(args.n)]
+	del prop_array[(len(prop_array)-args.n):len(prop_array)]
 	# TODO: Make list in list split on <s>
+	
+	prop_array = [prop_array[i:args.n+i] for i in range(len(prop_array)-(args.n))]
 
-	prop_array = [prop_array[i+1:args.n+i+1] for i in range(len(prop_array)-(args.n +1))]
-
-	print(prop_array)
+	# print(prop_array)
 
 	# prop_array = [prop_array[i*n: (i*n)+n] for i in range(n - 1)] 
 	# makes lists with lists of length n part 2
 
 	prop_dict = calc_prop(prop_array, model_n, model_n_min_one, args.n)
 	
-	print(prop_dict)
+	# print(prop_dict)
 
+	perm_array = ['I', 'do', 'not', 'know']
 
+	perms = tuple(itertools.permutations(perm_array)) #permutatie functie voor opdracht 4
 
+	start = tuple(['<s>'])
+	end = tuple(['</s>'])
 
-	# print(tuple(itertools.permutations(prop_array))) permutatie functie voor opdracht 4
+	perm_sen = [start*(args.n-1) + perms[i] + end*(args.n-1) for i in range(len(perms))]
 
-	# print(Counter(model_n).most_common(10)) 
+	print(perm_sen)
+	print(len(per_sen))
+	# # print(Counter(model_n).most_common(10)) 
 	#For part 1
 
 	# print(Counter(model_n_min_one).most_common(10)) 
 	#For part 1
-
-	
