9d5cf540286199af1a1e311dfa0b76ee75eab08f
Kaj Meijer <k.d.meijer17@gmail.com>
2016-03-11 13:28:51 +0100

    step 2 minus the v

--- b-step2.py
+++ b-step2.py
@@ -13,7 +13,7 @@ class Node:
 		
 
 
-def bin(sentences, output):
+def bin(sentences, output, h, v):
 	# Read all the lines of the input file
 	for i in sentences.readlines():
 		# Strips everything from the line to get the current sentence
@@ -44,12 +44,12 @@ def bin(sentences, output):
 				for _ in range(state.count(')')):
 					previousNode = previousNode.parent
 		# Make the output file with the created nodes of the input file		
-		writeTree(rootNode, 0, output)
+		write_tree(rootNode, 0, output, h, v)
 		# write \n\n to create an empty line in the output file 
 		output.write("\n\n")
 
 
-def writeTree(currentNode, nodeIndex, output):
+def write_tree(currentNode, nodeIndex, output, h, v):
 	# If the current 'node' is a node, process the node 
 	if isinstance(currentNode, Node):
 		# Writes (ROOT if the current node is ROOT
@@ -57,17 +57,29 @@ def writeTree(currentNode, nodeIndex, output):
 			output.write("(ROOT")
 		# If the parent has no previous children, print the current node 
 		elif currentNode.parent.children.index(currentNode) == 0:
-			output.write(" (" + currentNode.tag)
+			if isinstance(currentNode.children[0], Node):
+				parent_tag = make_parent_tag(currentNode.parent, v)
+				print(parent_tag)
+				output.write(" (" + parent_tag)
+			else: 
+				output.write(" (" + currentNode.tag)
 		# If the parent has previous children, print the parent and 
 		# all his children except the current node
 		else:
-			output.write(" (@" + currentNode.parent.tag + "->_" + 
-				'_'.join([c.tag for c in currentNode.parent.children[:nodeIndex]]) 
-				+ " (" + currentNode.tag)
+			parent_tag = make_parent_tag(currentNode, v)
+			number_of_siblings = len(currentNode.parent.children) - 1
+			if(number_of_siblings <= h and h >= nodeIndex):
+				output.write(" (@" + currentNode.parent.tag + "->_" + 
+					'_'.join([c.tag for c in currentNode.parent.children[:nodeIndex]]) 
+					+ " (" + currentNode.tag)
+			else: 
+				output.write(" (@" + currentNode.parent.tag + "->_" + 
+					'_'.join([c.tag for c in currentNode.parent.children[(nodeIndex-h):nodeIndex]]) 
+					+ " (" + currentNode.tag)
 		# Cycle to every child (depth first) to build the whole sentences the same way
 		# as you took them apart
 		for childIndex, childNode in enumerate(currentNode.children):
-			writeTree(childNode, childIndex, output)
+			write_tree(childNode, childIndex, output, h, v)
 	
 		output.write(")" * (len(currentNode.children)))
 	# If the current 'node'	is a string, write it in the output file
@@ -75,11 +87,21 @@ def writeTree(currentNode, nodeIndex, output):
 		
 		output.write(" " + currentNode)
 
+def make_parent_tag(currentNode, v):
+	parent_tag = ""
+	if(currentNode.parent != None):
+		parent_tag = currentNode.tag+ "^" + currentNode.parent.tag
+	return parent_tag
+
+
+
 if __name__ == '__main__':
 
 	# Gets input arguments
 	parser = argparse.ArgumentParser(add_help = False)
 
+	parser.add_argument("-h",  dest = 'h')
+	parser.add_argument("-v",  dest = 'v')
 	parser.add_argument("-input",  dest = 'input')
 	parser.add_argument("-output", dest = 'output')
 	args = parser.parse_args()
@@ -88,8 +110,12 @@ if __name__ == '__main__':
 	# Creates the nodes needed for the binarization
 	output = open(args.output, "w")
 
-	bin(input_file, output )
+	bin(input_file, output, int(args.h), int(args.v))
 
 
-				# print(niveau)	
+		# Check if the current node is a node, 
+		# then check for the number of 'v' what the parents are.
+			# Also check if there are that many parents, 
+			# only use 'v' if the amount of parents exceeds 'v'
+
 					
